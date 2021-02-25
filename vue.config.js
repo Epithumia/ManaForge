2e9c47b024deec01b55d0ec7ff53da97fc94d590
Rafael Lopez <lopez@airelle.info>
2021-02-25 18:48:51 +0100

    Build target dir

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/ManaForge/'
+        ? '/forge/'
         : '/',
     chainWebpack: config => {
         config.module
