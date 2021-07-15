6dbc41434675f10f7fade27f1da1d4f64a71ae87
Rafael Lopez <lopez@airelle.info>
2021-07-16 01:28:55 +0200

    Revert order of operations, otherwise Phoenix/etc breaks

--- src/forge/ForgedItem.js
+++ src/forge/ForgedItem.js
@@ -27,7 +27,6 @@ class ForgedItem {
             this.init_stat_limits();
             this.sub_init_cards();
             this.material_init();
-            this.check_type_effects();
         }
         if (seq.length > 2) {
             for (let i = 2; i < seq.length; i++) {
@@ -36,13 +35,13 @@ class ForgedItem {
                     this.init_stat_limits();
                     this.sub_init_cards();
                     this.material_init();
-                    this.check_type_effects();
                     this.set_active_world_card();
                     this.item_energy();
                     this.material_code();
                     this.item_code();
                     this.push_cards();
                     this.activate_cards();
+                    this.check_type_effects();
                     this.increase_essences();
                     this.calculate_stats();
                 }
