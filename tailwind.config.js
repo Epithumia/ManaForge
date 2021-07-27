a3fe9371fa9918a4c1e58a523af3ca814d5ee9f1
Rafael Lopez <lopez@airelle.info>
2021-07-27 12:53:49 +0200

    Custom breakpoints to improve display

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     ],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '840px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1333px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1600px',
+            // => @media (min-width: 1536px) { ... }
+
+            'full': '1920px',
+            // => @media (min-width: 1920px) { ... }
+
+        },
         extend: {
+            fontSize: {
+                'xxs': '.7rem'
+            },
             colors: {
                 night: {
                     100: '#919cae',
