3b74d3d2e8558d40b719f7ea4a96cc8231913652
Rafael Lopez <lopez@airelle.info>
2021-07-30 00:10:22 +0200

    Cap error in a file name

--- src/forge/items/wings_feathers/index.js
+++ src/forge/items/wings_feathers/index.js
@@ -2,4 +2,4 @@ export {Angel_Feather} from "@/forge/items/wings_feathers/Angel_Feather";
 export {Clear_Feather} from "@/forge/items/wings_feathers/Clear_Feather";
 export {Flaming_Quill} from "@/forge/items/wings_feathers/Flaming_Quill";
 export {Moth_Wing} from "@/forge/items/wings_feathers/Moth_Wing";
-export {Raven_Feather} from "@/forge/items/wings_feathers/raven_Feather";
+export {Raven_Feather} from "@/forge/items/wings_feathers/Raven_Feather";
