333963239dda00da8bd10574cc01fac43fa96401
Rafael Lopez <lopez@airelle.info>
2021-07-30 00:11:34 +0200

    Cap error in a file name

--- src/forge/items/stones_crystals/index.js
+++ src/forge/items/stones_crystals/index.js
@@ -4,5 +4,5 @@ export {Fire_Stone} from "@/forge/items/stones_crystals/Fire_Stone";
 export {Glow_Crystal} from "@/forge/items/stones_crystals/Glow_Crystal";
 export {Moon_Crystal} from "@/forge/items/stones_crystals/Moon_Crystal";
 export {Sun_Crystal} from "@/forge/items/stones_crystals/Sun_Crystal";
-export {Water_Stone} from "@/forge/items/stones_crystals/water_Stone";
+export {Water_Stone} from "@/forge/items/stones_crystals/Water_Stone";
 export {Wind_Stone} from "@/forge/items/stones_crystals/Wind_Stone";
