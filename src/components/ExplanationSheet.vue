bc543dfb6a8cc940f4616e896550b80f94af9ce7
Rafael Lopez <lopez@airelle.info>
2021-08-03 10:49:35 +0200

    Import some of the upcoming changes

---
+++ src/components/ExplanationSheet.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="explanations" ref="explanations">
+    <div v-if="explanations.explain_init_stat_limits" v-html="explanations.explain_init_stat_limits">
+    </div>
+    <div v-if="explanations.explain_sub_init_cards" v-html="explanations.explain_sub_init_cards">
+    </div>
+    <div v-if="explanations.explain_material_init" v-html="explanations.explain_material_init">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExplanationSheet",
+  props: {'explanations' : Object}
+}
+</script>
+
+<style scoped>
+
+</style>
